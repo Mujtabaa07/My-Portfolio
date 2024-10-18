f682b766eb218229233062058a681392ddc45994
Mohamed Mujtaba <mohamedmujtaba07@gmail.com>
2024-10-18 14:30:44 +0530

    next file corrected

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-};
-
-export default nextConfig;
+    images: {
+      // Add the domain of the external image source
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
